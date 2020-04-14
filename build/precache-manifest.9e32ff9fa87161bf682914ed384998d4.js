b15839331c62dbb172ee4ba678ad3f0edfc6ac4b
Hang Ho <bemoon0103@gmail.com>
2020-04-13 23:33:56 -0700

    add build

---
+++ build/precache-manifest.9e32ff9fa87161bf682914ed384998d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0558cd6e5d1064dc0c5763291bc14225",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf7ae861230c2da38b82",
+    "url": "/static/css/main.7066daa1.chunk.css"
+  },
+  {
+    "revision": "6cc55eb36b7391fae420",
+    "url": "/static/js/2.4ca48071.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4ca48071.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf7ae861230c2da38b82",
+    "url": "/static/js/main.9c3aaf94.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
